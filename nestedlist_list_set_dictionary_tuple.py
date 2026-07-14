5f388c7bb48de3c634af86100c96ec75e99783dd
SriBalajiMandala <sribalajimandala@gmail.com>
2026-07-14 10:52:25 +0530

    Add files via upload

---
+++ nestedlist_list_set_dictionary_tuple.py
@@ -0,0 +1,36 @@
+#list
+fruits = ["Apple", "Banana", "Mango"]
+print(fruits)
+
+fruits.append("Orange")
+print(fruits)
+
+#Nested list
+students = [
+    ["Raju", 20],
+    ["Ravi", 21],
+    ["Anu", 19]
+]
+
+print(students)
+print(students[1][0])  
+
+#Tuple
+colors = ("Red", "Green", "Blue")
+print(colors)
+print(colors[1])
+
+#sets
+nums = {10, 20, 20, 30, 40}
+print(nums)
+
+#Dictionary
+student = {
+    "name": "Raju",
+    "age": 20
+}
+
+print(student["name"])
+student["age"] = 21
+print(student)
+
